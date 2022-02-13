95a8a435aa0e1f2e0a439afb19f4af5833daee62
James Fan <jmfan2002@gmail.com>
2022-02-13 12:10:26 -0500

    Updates

---
+++ precache-manifest.674ad9292d9d2bb6fce03844a4a6027b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "230fa180387b95cde02ba47c7b664fbf",
+    "url": "/HTN-application/index.html"
+  },
+  {
+    "revision": "b2c25b2f0b4195145641",
+    "url": "/HTN-application/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "e4c660f32c3eaa39e220",
+    "url": "/HTN-application/static/css/main.a44d7bb3.chunk.css"
+  },
+  {
+    "revision": "b2c25b2f0b4195145641",
+    "url": "/HTN-application/static/js/2.ea2bc70f.chunk.js"
+  },
+  {
+    "revision": "37dac347f884d0b9d731",
+    "url": "/HTN-application/static/js/3.f01659bd.chunk.js"
+  },
+  {
+    "revision": "e4c660f32c3eaa39e220",
+    "url": "/HTN-application/static/js/main.b9b46ccf.chunk.js"
+  },
+  {
+    "revision": "6555bf8951dc78b8a34b",
+    "url": "/HTN-application/static/js/runtime~main.89009677.js"
+  },
+  {
+    "revision": "9bc0fd5e975eb95fbf5897e341668475",
+    "url": "/HTN-application/static/media/htn_logo.9bc0fd5e.png"
+  }
+]);
